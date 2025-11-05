2f77756cb4913e0df19d0a03abc20f22d67fec5d
Hoàng Minh Đức <minhduchalai4@gmail.com>
2025-11-05 15:34:27 +0700

    update clean architecture and logo

---
+++ client/lib/presentation/splash/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+import '../home/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const HomeScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          'assets/images/trekka_logo.png',
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}
